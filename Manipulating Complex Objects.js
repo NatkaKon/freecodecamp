a9d41bcc3837b2d6cba12cb53f028eacf589cb5d
Natalia Kondratieva <kondratieva07@gmail.com>
2022-05-13 20:45:48 +0300

    feat: test done

---
+++ Manipulating Complex Objects.js
@@ -0,0 +1,25 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "ABBA",
+      "title": "Hello",
+      "release_year": 1981,
+      "formats": [
+        "CD",
+        "DVD",
+        "Video"
+      ],
+    }
+  ];
+  
+  
